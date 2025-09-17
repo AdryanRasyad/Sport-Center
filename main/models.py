acdbbe37ad4b6a190c1f8aca4643fffcc99dca1b
AdryanRasyad <adryanrasyad@gmail.com>
2025-09-17 07:58:52 +0700

    Tugas 3

--- main/models.py
+++ main/models.py
@@ -8,13 +8,17 @@ class Product(models.Model):
         ('accessories', 'Accessories'),
         ('limited_collection', 'Limited Collection'),
     ]
-    
+
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     
     def __str__(self):
         return self.title
